4e8bf3408db51812559d246370b0afd736a6df5d
D2 <darrendelimond@gmail.com>
2024-06-17 05:20:43 -0400

    =starting out easy

---
+++ heyProgrammer/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        Source s = new Source();
+        System.out.println(s.greet("Darren"));
+    }
+}
---
+++ heyProgrammer/src/Source.java
@@ -0,0 +1,6 @@
+class Source {
+    public String greet(String wrd)
+    {
+       return "Hey "+wrd;
+    }
+}
