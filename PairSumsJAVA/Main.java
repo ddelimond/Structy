b96c95ea4247357483cf5acc640633beee9cfa75
D2 <darrendelimond@gmail.com>
2024-08-14 07:21:42 -0400

    =completed JAVA PairSums

--- PairSumsJAVA/Main.java
+++ PairSumsJAVA/Main.java
@@ -27,7 +27,7 @@ public class Main {
     public static void main (String[] args){
 
 
-        System.out.println(pairSum(List.of(3, 2, 5, 4, 1), 8));
+        pairSum(List.of(3, 2, 5, 4, 1), 8);
 
     }
 }
