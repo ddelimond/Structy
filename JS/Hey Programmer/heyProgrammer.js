9bf96fa086b691df45dddcc10a44b5f5a09e4fb8
Darren Delimond <darrendelimond@gmail.com>
2025-07-28 22:34:59 -0400

    =completed challenge

---
+++ JS/Hey Programmer/heyProgrammer.js
@@ -0,0 +1,12 @@
+
+
+/*
+Write a function greet that takes in a string argument, s,
+and returns the string "hey s".
+No tricks here. Run the tests to check your work.
+ */
+
+const greet = (s) => {
+    // todo
+    return `hey ${s}`;
+};
